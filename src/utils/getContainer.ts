39a04b2f4d54ac44e98020216048c230d816fb96
angular-moon <xiaoxuss@gmail.com>
2023-08-16 17:15:18 +0800

    getTopRoot support shadowRoot

--- src/utils/getContainer.ts
+++ src/utils/getContainer.ts
@@ -1,11 +1,15 @@
-/* eslint-disable no-restricted-globals */
 /**
  * 顶层窗口引用挂载节点
  */
 export const getTopRoot = () => {
   // try 防止跨域报错
   try {
-    return (top?.document.querySelector('#mount-root') as HTMLElement) || top.document.body;
+    return (
+      ((window?.top?.document.querySelector('#mount-root')
+        ?.shadowRoot as unknown) as HTMLElement) ||
+      ((window?.top?.document.querySelector('#mount-root') as unknown) as HTMLElement) ||
+      window?.top?.document.body
+    );
   } catch (error) {
     return document.body;
   }
